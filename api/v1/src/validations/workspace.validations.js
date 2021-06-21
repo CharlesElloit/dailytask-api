f2e749728f626ff294d14e07b9c8ed33a8cba38c
CharlesElloit <aciforcharles@gmail.com>
2021-06-21 00:02:06 -0700

    added update workspace route validation

---
+++ api/v1/src/validations/workspace.validations.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+exports.validateWorkspace = (data) => {
+  const schema = Joi.object({
+    title: Joi.string().required().trim().strip().empty().max(100).min(3),
+  })
+  return schema.validate(data);
+}
+
+
