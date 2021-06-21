ffcf159198fd8ab4ac56bf763e540e05601475c3
CharlesElloit <aciforcharles@gmail.com>
2021-06-20 23:58:09 -0700

    added updated workspace route

--- api/v1/src/routes/index.js
+++ api/v1/src/routes/index.js
@@ -2,8 +2,9 @@ const router = require("express").Router();
 const { catchErrors } = require("../handlers/apiErrorHandler");
 const workspaceController = require("../controllers/workspace");
 
-router.post("/workspace/add", catchErrors(workspaceController.createWorkspace));
-router.get("/workspaces", catchErrors(workspaceController.getAllWorkspaces))
+router.post("/workspaces/add", catchErrors(workspaceController.createWorkspace));
+router.get("/workspaces", catchErrors(workspaceController.getAllWorkspaces));
+router.put("/workspace/:id", catchErrors(workspaceController.updateWorkspace));
 router.delete("/workspace/:id", catchErrors(workspaceController.deleteWorkspace));
 
 module.exports = router;
