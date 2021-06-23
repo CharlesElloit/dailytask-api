484105d86e422fd1612f241bafe24df83c9fb32c
CharlesElloit <aciforcharles@gmail.com>
2021-06-22 20:38:18 -0700

    added createWorkspace test cases

--- api/v1/src/controllers/workspace/createWorkspace.test.js
+++ api/v1/src/controllers/workspace/createWorkspace.test.js
@@ -1,5 +1,16 @@
-describe("Sample Test", () => {
-  it("Should test that true === true", () => {
-    expect(true).toBe(true);
+const request = require("supertest");
+const app = require("../../index");
+
+describe("Post Endpoints", () => {
+  beforeEach(() => jest.setTimeout(30000));
+
+  it("Should create a new workspace", async () => {
+    const res = await request(app)
+      .post("/workspaces/add")
+      .send({
+        title: "test is cool",
+      });
+    expect(res.statusCode).toEqual(201);
+    expect(res.body.success).toBe(true);
   });
 });
