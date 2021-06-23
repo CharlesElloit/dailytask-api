7104869113b0b108f6a75185fb355d7b8ad7cc40
CharlesElloit <aciforcharles@gmail.com>
2021-06-23 00:30:27 -0700

    updated MONGODB_URI

--- api/v1/src/index.js
+++ api/v1/src/index.js
@@ -12,15 +12,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-let DBConnectionUri;
-
-if (process.env.NODE_ENV === "development") {
-  DBConnectionUri = process.env.LOCAL_MONGODB_URI;
-} else {
-  DBConnectionUri = process.env.MONGODB_URI;
-}
-
-DBConnection(DBConnectionUri);
+DBConnection(process.env.MONGODB_URI);
 
 app.use("/", appRoutes);
 
