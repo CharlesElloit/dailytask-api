b43adf26976db778c338fbe94f90125ede1c59f9
CharlesElloit <aciforcharles@gmail.com>
2021-06-28 16:43:48 -0700

    added signup routes

--- api/v1/src/routes/index.js
+++ api/v1/src/routes/index.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const { catchErrors } = require("../handlers/apiErrorHandler");
+const userController = require("../controllers/user");
 const workspaceController = require("../controllers/workspace");
+const projectController = require("../controllers/project");
 
+// user routes
+router.post("/signup", catchErrors(userController.signup));
+// workspace routes
 router.post("/workspaces/add", catchErrors(workspaceController.createWorkspace));
 router.get("/workspaces", catchErrors(workspaceController.getAllWorkspaces));
 router.put("/workspace/:id", catchErrors(workspaceController.updateWorkspace));
 router.delete("/workspace/:id", catchErrors(workspaceController.deleteWorkspace));
+router.get("/search", catchErrors(workspaceController.searchWorkspace));
+router.post("/projects/add", catchErrors(projectController.createProject));
 
 module.exports = router;
