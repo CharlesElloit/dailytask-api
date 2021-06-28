fdfa11303157481e9064c92a81f3ed5730747f73
CharlesElloit <aciforcharles@gmail.com>
2021-06-28 11:03:24 -0700

    added User schema definition

---
+++ api/v1/src/models/User.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    email: true,
+    max: 255,
+    lowercase: true,
+  },
+  username: {
+    type: String, required: true, trim: true, unique: true,
+  },
+  avatar: {
+    type: String,
+    default: "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg",
+  },
+  password: {
+    type: String, required: true, max: 255,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
