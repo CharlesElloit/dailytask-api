295250bcb00082139cb653bc37dc07ce783541f3
CharlesElloit <aciforcharles@gmail.com>
2021-06-28 16:37:24 -0700

    added signup validations

---
+++ api/v1/src/validations/signup.validator.js
@@ -0,0 +1,21 @@
+const Joi = require("joi");
+
+const validateSignup = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().required().trim().email()
+      .empty()
+      .max(255),
+    username: Joi.string().required().trim().strip()
+      .min(3)
+      .max(60)
+      .empty(),
+    password: Joi.string().min(6).max(30).required()
+      .trim()
+      .strip()
+      .empty()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+  });
+  return schema.validate(data);
+};
+
+module.exports = validateSignup;
