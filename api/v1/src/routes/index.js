36d0a96fd39d6ba3b52226e6a5e06ad221ea50d9
CharlesElloit <aciforcharles@gmail.com>
2021-07-02 13:01:36 -0700

    added authorization middleware for getAllWorkspace and createWorkspace

--- api/v1/src/routes/index.js
+++ api/v1/src/routes/index.js
@@ -3,13 +3,14 @@ const { catchErrors } = require("../handlers/apiErrorHandler");
 const userController = require("../controllers/user");
 const workspaceController = require("../controllers/workspace");
 const projectController = require("../controllers/project");
+const { auth } = require("../middlewares/auth");
 
 // user routes
 router.post("/signup", catchErrors(userController.signup));
 router.post("/signin", catchErrors(userController.signin));
 // workspace routes
-router.post("/workspaces/add", catchErrors(workspaceController.createWorkspace));
-router.get("/workspaces", catchErrors(workspaceController.getAllWorkspaces));
+router.post("/workspaces/add", auth, catchErrors(workspaceController.createWorkspace));
+router.get("/workspaces", auth, catchErrors(workspaceController.getAllWorkspaces));
 router.put("/workspace/:id", catchErrors(workspaceController.updateWorkspace));
 router.delete("/workspace/:id", catchErrors(workspaceController.deleteWorkspace));
 router.get("/search", catchErrors(workspaceController.searchWorkspace));
