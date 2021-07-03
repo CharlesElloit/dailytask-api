03565e98e9ce19571cc45e86c61e127a417f0d09
CharlesElloit <aciforcharles@gmail.com>
2021-07-03 07:50:57 -0700

    updated .env.sample

--- api/v1/src/controllers/workspace/index.js
+++ api/v1/src/controllers/workspace/index.js
@@ -1,6 +1,8 @@
 module.exports = {
   createWorkspace: require("./createWorkspace"),
   getAllWorkspaces: require("./getAllWorkspace"),
+  getWorkspace: require("./getWorkspace"),
   updateWorkspace: require("./updateWorkspace"),
   deleteWorkspace: require("./deleteWorkspace"),
+  searchWorkspace: require("./searchWorkspace"),
 };
